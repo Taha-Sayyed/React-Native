53b81d003274f66736d9f99f8c4bf99f93d0254b
Taha Sayyed <codetaha786@gmail.com>
2026-03-14 19:30:06 +0530

    feat: add basic App component rendering Hello World text in SafeAreaView

---
+++ Awesome01/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+function App(){
+  return(
+    <SafeAreaView>
+    <View>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+    </View>
+  </SafeAreaView>
+  )
+  
+}
+
+export default App;
