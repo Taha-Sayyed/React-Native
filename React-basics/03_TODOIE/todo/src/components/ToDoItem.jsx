c4e0211a3beec59dd2c8dadb9080acccd5d2d67e
Taha Sayyed <codetaha786@gmail.com>
2026-03-14 19:51:51 +0530

    feat: add ToDoItem component for todo list UI

---
+++ React-basics/03_TODOIE/todo/src/components/ToDoItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ToDoItem=()=>{
+    return(
+        <li className="todo-item">
+            <span>
+                <input type="checkbox" />
+                <span>Eat</span>
+            </span>
+            <p>...</p>
+            
+        </li>
+    );
+}
+
+export default ToDoItem
