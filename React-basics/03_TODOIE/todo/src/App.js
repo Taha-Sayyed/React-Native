97b03b92f9527401a27ee4e54e4b0589304e25ee
Taha Sayyed <codetaha786@gmail.com>
2026-03-14 19:52:59 +0530

    feat: compose App component with Header, ToDoItem, and Button components

---
+++ React-basics/03_TODOIE/todo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import ToDoItem from './components/ToDoItem';
+import Button from './components/Button';
+import "./style.css";
+
+function App() {
+  return (
+    <div className='todo-container'>
+      <Header/>
+      <ToDoItem/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
