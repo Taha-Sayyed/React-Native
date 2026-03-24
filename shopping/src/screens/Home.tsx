b6394943ed13bd385ee6e039be7ac9df7f8b4761
Taha Sayyed <codetaha786@gmail.com>
2026-03-24 23:03:14 +0530

    Screens

---
+++ shopping/src/screens/Home.tsx
@@ -0,0 +1,53 @@
+import { View, Text,StyleSheet, Pressable, FlatList} from 'react-native'
+import React from 'react'
+
+//Navigation
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+import { RootStackPramList } from '../App'
+
+//Components
+import ProductItem from '../components/ProductItem'
+import Separator from '../components/Separator'
+
+//data
+import { PRODUCTS_LIST } from '../data/constants'
+
+
+type HomeProps=NativeStackScreenProps<RootStackPramList,'Home'>
+
+
+const Home = ({navigation}:HomeProps) => {
+  return (
+    <View>
+      <FlatList
+        data={PRODUCTS_LIST}
+        keyExtractor={item=>item.id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({item})=>(
+          <Pressable
+            onPress={()=>{
+              navigation.navigate('Details',{
+                product:item
+              })
+            }}
+          >
+            <ProductItem product={item}/>
+          </Pressable>
+        )}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+});
+
+export default Home
